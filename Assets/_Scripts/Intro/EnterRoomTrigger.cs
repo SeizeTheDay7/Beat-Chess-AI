937ceaa021a7953934b1a69893dc998b26bdde1c
lazyArtisan <biomatrix117@naver.com>
2025-03-16 20:43:20 +0900

    입구까지 연출까지 얼추 완성

--- Assets/_Scripts/Intro/EnterRoomTrigger.cs
+++ Assets/_Scripts/Intro/EnterRoomTrigger.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using System.Collections;
+using NUnit.Framework.Constraints;
 
 public class EnterRoomTrigger : MonoBehaviour
 {
     [SerializeField] private MetalDoor metalDoor;
+    [SerializeField] private float waitTime = 2.0f;
+    [SerializeField] private GameObject spotlight;
+    [SerializeField] private ComputerGlitter computerGlitter;
+    [SerializeField] private GameObject computerScreen;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private GameObject frontDeskTrigger;
     private bool isTriggered = false;
+
+    void Start()
+    {
+        computerScreen.SetActive(false); // 빌드에는 없어도 됨
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -17,6 +29,13 @@ public class EnterRoomTrigger : MonoBehaviour
     {
         float doorCloseTime = metalDoor.MetalDoorClose();
         yield return new WaitForSeconds(doorCloseTime);
+        yield return new WaitForSeconds(waitTime);
+        spotlight.SetActive(true);
+        audioSource.Play();
+        computerGlitter.enabled = true;
+        computerScreen.SetActive(true);
+        yield return new WaitForSeconds(5.0f);
+        frontDeskTrigger.SetActive(true);
         Destroy(gameObject);
     }
 }
